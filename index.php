84fb0e7435071841da063ccd5e027294d3547ec6
Princess413 <35474633+Princess413@users.noreply.github.com>
2018-02-03 13:32:50 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Workshop2-Alcontin</title>
-</head>TO BE UPLOADED LATER!!!
+	<title>Networking</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
 <body>
-
-</body>
-</html>
+<div class = "mainBase">
+	<div class = "centerDiv">
+		<!--Second Layer | Pager-->
+		<div class = "headerDiv">
+			<table>
+				<tr>
+					<td><img style="position: relative; top: 0px;margin: 0px;padding: 0px;" src="img/home.png" width="100px" height="90px"></td>
+					<td>
+						<img style="position: relative;top:0px;padding: 0px;" src="img/kai.png" width="590px" height="100px">
+					</td>
+				</tr>
+			</table>
+		</div>
+		<!--Menu Item and Search Bar-->
+		<div class = "menuItem">
+			<a style="float:left"; class = "linker" href="php/About.php" id = "about">About</a>
+			<a style="float:left"; class = "linker" href="php/cabling.php" id = "profile">Cabling Types</a>
+			<a style="float:left"; class = "linker" href="php/fiber.php" id = "contact" href="">Fiber Optics Connectors</a>
+			<a style="float:left"; class = "linker" href="php/packet.php" id = "signup" href=""> --CISCO  PT--</a>
+		</div>
+		
+	</div>
+	<img src="img/Base.jpg" width="100%" height="100%">
+	<!--Login Base-->
